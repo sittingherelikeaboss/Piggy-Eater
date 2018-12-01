8ec5438defea528fb9b30bc69beb3fd8720bb731
Alex <alex.szebrag.mcmaster@gmail.com>
2018-12-01 12:04:30 -0500

    testing if phone works with google play services

--- app/src/main/java/com/example/total/piggyeater/MainActivity.java
+++ app/src/main/java/com/example/total/piggyeater/MainActivity.java
@@ -9,8 +9,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        GoogleApiAvailability api = GoogleApiAvailability.getInstance();
+        int code = api.isGooglePlayServicesAvailable(activity);
+        if (code == ConnectionResult.SUCCESS) {
+            // Do Your Stuff Here
+        } else {
+            AlertDialog alertDialog =
+                    new AlertDialog.Builder(activity, R.style.AppCompatAlertDialogStyle).setMessage(
+                            "You need to download Google Play Services in order to use this part of the application")
+                            .create();
+            alertDialog.show();
+        }
 
 
 
