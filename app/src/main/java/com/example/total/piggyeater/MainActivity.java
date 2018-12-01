a8f09a350f9c4d6024532fd5b898af1eee2b4604
jastine <goyenaja@mcmaster.ca>
2018-12-01 12:46:45 -0500

    Added toast notification to check if Google Play Services is connected

--- app/src/main/java/com/example/total/piggyeater/MainActivity.java
+++ app/src/main/java/com/example/total/piggyeater/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.total.piggyeater;
 
+import android.content.Context;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
@@ -12,10 +14,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
         GoogleApiAvailability api = GoogleApiAvailability.getInstance();
         int code = api.isGooglePlayServicesAvailable(this);
         if (code == ConnectionResult.SUCCESS) {
-            // Do Your Stuff Here
+            Context context = getApplicationContext();
+            CharSequence text = "Connected!";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
         } else {
             AlertDialog alertDialog =
                     new AlertDialog.Builder(this, R.style.MyAlertDialogStyle).setMessage(
